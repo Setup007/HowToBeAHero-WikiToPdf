c6197990fa7b77d9d747692c6ebee8fd9e9056d9
Tom Kirchgäßner <tomkirchgaessner@Toms-MBP.fritz.box>
2018-02-23 15:36:06 +0100

    further changes to template to match html-pdf module

--- html-pdf/html2pdf.js
+++ html-pdf/html2pdf.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('../template/template-frame_html.html', 'utf8');
 var options = {
-    format: 'A4',
+    width: "210mm",
+    height: "297mm",
     timeout: '50000',
     base: 'file://' + path.resolve('./') + '/'
 };
-console.log('file://' + path.resolve('./') + '/');
 
 pdf.create(html, options).toFile('./Test3.pdf', function(err, res) {
   if (err) return console.log(err);
