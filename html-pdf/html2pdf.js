ca00b1657e769cdcdcd82eb35e79fc33b78f4f56
Tom Kirchgäßner <tomkirchgaessner@Toms-MBP.fritz.box>
2018-02-23 14:40:24 +0100

    template updates

--- html-pdf/html2pdf.js
+++ html-pdf/html2pdf.js
@@ -1,9 +1,15 @@
+var path = require('path');
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('../template/template-frame_html.html', 'utf8');
-var options = { format: 'Letter' };
+var options = {
+    format: 'A4',
+    timeout: '50000',
+    base: 'file://' + path.resolve('./') + '/'
+};
+console.log('file://' + path.resolve('./') + '/');
 
-pdf.create(html, options).toFile('./test.pdf', function(err, res) {
+pdf.create(html, options).toFile('./Test3.pdf', function(err, res) {
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
