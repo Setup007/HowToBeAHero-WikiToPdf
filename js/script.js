a95f72a03fc2f4714246bf866601dbcd234468b9
Setup <>
2018-07-09 19:01:57 +0200

    Seems to be the right way. Some weird shit still happening with the pages though.

--- js/script.js
+++ js/script.js
@@ -2,30 +2,16 @@ $(document).ready(function() {
     //do some splitting of rendered pages here
     debugger;
     let lastId = $('.page').last().attr('id');
-
+    let splitPartCount =0;
     for (let i = 0; i < lastId; i++) {
-        let splitContentId = '#' + i;
+        let pageNumber = i;
+        // splitPartCount =0;
         //go through every page and grab content element and its children
 
         let contentElements = $('#' + i).children('.content').children();
         let element = $('#' + i).children('.content')[0];
 
-        for (let j = 0; j < contentElements.length; j++) {
-            if (contentElements[j].offsetTop + contentElements[j].offsetHeight > element.offsetTop + element.offsetHeight ||
-                contentElements[j].offsetLeft + contentElements[j].offsetWidth > element.offsetLeft + element.offsetWidth) {
-                //this item is semi visible, clone a new template and put everything there
-
-                $('#template').clone().prop('id', i+"-"+j).insertAfter($(splitContentId));
-                splitContentId='#'+i+"-"+j;
-                //starting with this element move everything to the cloned page
-                $(splitContentId).children('.content').html($(element).children().slice(j));
-                return;
-                //check if content would fit cloned page
-                //element.children[j];
-                //
-            }
-
-        }
+       splitContent(pageNumber, element, contentElements);
         // contentElements.each(function(index, element) {
         //     console.log("childs height: " +  $(this).outerHeight(true)  + " height of page: " +  $('#' + i).outerHeight(true));
         //     if ( $(this)[0].outerHeight(true) > $('#' + i).outerHeight(true)) {
@@ -37,4 +23,29 @@ $(document).ready(function() {
     //remove template
     $('#template').remove();
     //send new html to the nodejs
+    function splitContent(pageNumber, element, contentElements){
+
+        for (let j = 0; j < contentElements.length; j++) {
+            if (contentElements[j].offsetTop + contentElements[j].offsetHeight > element.offsetTop + element.offsetHeight ||
+                contentElements[j].offsetLeft + contentElements[j].offsetWidth > element.offsetLeft + element.offsetWidth) {
+
+                //this item is semi visible, clone a new template and put everything there
+                let splitContentId= splitPartCount === 0? pageNumber: pageNumber+"-"+splitPartCount;
+                splitPartCount++;
+                $('#template').clone().prop('id', pageNumber+"-"+splitPartCount).insertAfter($('#'+splitContentId));
+                splitContentId= pageNumber+"-"+splitPartCount;
+                //starting with this element move everything to the cloned page
+                $('#'+splitContentId).children('.content').first().html($(element).children().slice(j));
+                //check if content would fit cloned page
+                // $(element).children().slice(j).appendTo($('#'+splitContentId).children('.content'));
+
+                splitContent(pageNumber, $('#'+splitContentId).children('.content')[0], $('#'+splitContentId).children('.content').children());
+                break;
+                //
+                //element.children[j];
+                //
+            }
+
+        }
+    }
 });
