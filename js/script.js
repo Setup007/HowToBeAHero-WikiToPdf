af6809653701f40248e67f231cb56aa0e79dbf74
Setup <>
2018-07-10 13:17:51 +0200

    Fixed pdf style. Added page numbers.

--- js/script.js
+++ js/script.js
@@ -5,21 +5,23 @@ $(window).on('load', function() {
     //do some splitting of rendered pages here
     let lastId = $('.page').last().attr('id');
     let splitPartCount = 0;
+    let pageCount =1;
     for (let i = 0; i < lastId + 1; i++) {
-        let pageNumber = i;
+        let pageId = i;
         splitPartCount = 0;
         //go through every page and grab content element and its children
 
         let contentElements = $('#' + i).children('.content').children();
         let element = $('#' + i).children('.content')[0];
-
-        splitContent(pageNumber, element, contentElements);
+        $('#'+pageId).find('.pageCount').text(pageCount);
+        splitContent(pageId, element, contentElements);
         // contentElements.each(function(index, element) {
         //     console.log("childs height: " +  $(this).outerHeight(true)  + " height of page: " +  $('#' + i).outerHeight(true));
         //     if ( $(this)[0].outerHeight(true) > $('#' + i).outerHeight(true)) {
         //         console.log("is outside! "+'#' + i);
         //     }
         // });
+        pageCount++;
     }
     // $('#template').clone().prop('id', j).appendTo('body');
     //remove template
@@ -60,6 +62,9 @@ $(window).on('load', function() {
                 splitContentId = pageNumber + "-" + splitPartCount;
                 //set title of cloned page
                 $('#' + splitContentId).children('.title').text(element.parentElement.getElementsByClassName('title')[0].textContent);
+                //set page count
+                pageCount++;
+                $('#'+splitContentId).find('.pageCount').text(pageCount);
                 //starting with this element move everything to the cloned page
                 if (j === 0) {
                     $('#' + splitContentId).children('.content').first().html($(element).children().slice(1));
